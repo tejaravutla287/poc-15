1c4c0d60244f7805db2c2b048dfab647324d050f
pavankattamuri8 <147482466+pavankattamuri8@users.noreply.github.com>
2026-06-16 11:28:14 +0530

    Create main.tf

---
+++ AKS/main.tf
@@ -0,0 +1,21 @@
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "aks-resource-group"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = "my-aks-cluster"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "myakscluster"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_D2s_v3"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
