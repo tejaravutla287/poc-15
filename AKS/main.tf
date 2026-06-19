73ec5a79c09a1f50e0a9af6f8e9de8fa7a3e20da
tejaravutla287 <tejaravutla287@gmail.com>
2026-06-19 12:17:08 +0530

    Update main.tf

--- AKS/main.tf
+++ AKS/main.tf
@@ -2,19 +2,23 @@ resource "azurerm_resource_group" "aks_rg" {
   name     = "poc-15"
   location = "West US"
 }
-
+ 
 resource "azurerm_kubernetes_cluster" "aks_cluster" {
   name                = "my-aks-cluster"
   location            = azurerm_resource_group.aks_rg.location
   resource_group_name = azurerm_resource_group.aks_rg.name
   dns_prefix          = "myakscluster"
-
+ 
   default_node_pool {
     name       = "default"
-    node_count = 2
-    vm_size    = "Standard_D2s_v7"
+    node_count = 1
+    vm_size    = "Standard_B2ps_v2"
+ 
+    upgrade_settings {
+      max_surge = "0"
+    }
   }
-
+ 
   identity {
     type = "SystemAssigned"
   }
